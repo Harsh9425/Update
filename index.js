78bafa294ae7a2072209e7806c030685a0292885
Harsh9425 <harsh.web19@gmail.com>
2021-07-18 14:37:35 +0530

    changed

--- index.js
+++ index.js
@@ -16,9 +16,20 @@ client.connect(() => {
     console.log("connected to DAtabase");
 });
 
-let date = new Date();
-// ----------------------------------- timeout 1 ------------------------------------------------------
-setTimeout(() => {
+app.post("/test", (req, res) => {
+  if(req.body.key==process.env.key)
+  {
+    res.sendStatus(200)
+  }
+  else
+  {
+    res.sendStatus(400);
+  }
+});
+
+app.post("/update", (req, res) => {
+  if(req.body.key==process.env.key)
+  {
     let date = new Date();
     let dd = date.getDate();
     let mm = date.getMonth()+1;
@@ -31,39 +42,16 @@ setTimeout(() => {
         .then((database_res)=>{
           client.query(`UPDATE slots SET date = date + 7 where date = '${yyyy}-${mm}-${dd}'`)
           .then((database_res)=>{
-
+             res.sendStatus(200);
           })
         }) 
       })
     })
-}, 1626546300000 - date.getTime());
-// --------------------------------- timeout 1 end-----------------------------------------------------
-
-// ----------------------------------- timeout 2 ------------------------------------------------------
-setTimeout(() => {
-  setInterval(() => {
-    let date = new Date();
-    let dd = date.getDate();
-    let mm = date.getMonth()+1;
-    let yyyy = date.getFullYear();
-    client.query(`UPDATE movies SET booked = 0 WHERE date = '${yyyy}-${mm}-${dd}'`)
-    .then((database_res)=>{
-      client.query(`UPDATE available SET date = date + 7 where date = '${yyyy}-${mm}-${dd}'`)
-      .then((database_res)=>{
-        client.query(`UPDATE movies SET date = date + 7 where date = '${yyyy}-${mm}-${dd}'`)
-        .then((database_res)=>{
-          client.query(`UPDATE slots SET date = date + 7 where date = '${yyyy}-${mm}-${dd}'`)
-          .then((database_res)=>{
-
-          })
-        }) 
-      })
-    })
-
-  }, 86400000);
-}, 1626546360000 - date.getTime());
-// -------------------------------- timeout 2 end ---------------------------------------------------
-
+  }
+  else{
+    res.sendStatus(400);
+  }
+});
 app.listen(port, () => {
     console.log(`Server is Running on port ${port}`);
 });
